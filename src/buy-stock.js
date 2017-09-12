435dad58d2168baa00972f9d3a2a4dd137c9f26a
Sejal Varshney <varshney.sejal@gmail.com>
2017-09-12 11:39:45 +0530

    Add files via upload

---
+++ src/buy-stock.js
@@ -0,0 +1,7 @@
+const buy-stock = (repo1, repo2) => {
+  return Promise.resolve(
+    { type: 'text', content: `${company} and ${stock}` stocks }
+  )
+}
+
+module.exports = buy-stock
---
+++ src/config.js
@@ -0,0 +1,5 @@
+process.env.PORT = '5000'
+process.env.REQUEST_TOKEN = '2bf627ce213ba17013432838858c6b19'
+process.env.LANGUAGE = 'en'
+// Write your own configuration here:
+// ...
